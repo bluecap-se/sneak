dec94c7d82aa789fe87e40454f2670f820fe46c6
Andrej Babic <hello@bluecap.se>
2018-08-02 22:00:20 +0200

    Docs: Typo

--- sneak/parser.py
+++ sneak/parser.py
@@ -6,7 +6,7 @@ Sneak: A terminal snake game
 Usage: run.py [options]
 
 Options:
-  -h, --help            show this help message and exit
+  -h, --help            Show this help message and exit
   -s SIZE, --size=SIZE  Game size (s | m | l)
 
 """
